fc344e1b460bced944025c90db8c6307cf41b816
Dawid Fourie <dawief4@gmail.com>
2023-03-07 21:59:32 -0800

    sixth commit

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const currentMailboxText = document.querySelector(".Current-Mailbox");
 const searchAnim = () => {
     if (searchBarAnim.classList.contains('visually-hidden')){
         searchBarAnim.classList.remove('visually-hidden');
-        console.log("visible")
     }
     else {
         searchBarAnim.classList.add('visually-hidden');
